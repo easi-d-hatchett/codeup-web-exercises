ae78cea368ae48ac24e1f9c473c493e1ce2a4fab
Easi Hatchett <easi.hatchett3@gmail.com>
2023-11-20 11:55:17 -0500

    to-do-list

--- to-do-list.js
+++ to-do-list.js
@@ -1,15 +1,35 @@
 "use strict";
 
 (() => {
-    const form = document.querySelector("#to-do-form")
-    const addBtn =document.querySelector("#to-do-form button")
+    const form = document.querySelector("#to-do-form");
+    const addBtn =document.querySelector("#to-do-form button");
 
     form.addEventListener("submit", e => {
         e.preventDefault();
-        const input =document.querySelector("#to-do");
-        const userInput
+        const input = document.querySelector("#to-do");
+        const toDOList = document.querySelector("#to-do-list");
+        const userInput = input.value;
+        input.value = "";
 
-    }
+        const Li = document.createElement("li");
+        const p = document.createElement("p");
+        const doneBtn = document.createElement("button");
 
+        p.classList.add("m-0");
+        doneBtn.classList.add("btn", "btn-danger");
+        li.classList.add("to-do-item", "list-group-item", "d-flex", "justify-conten-between", "align-items-center");
+
+        doneBtn.innerText = "Done";
+        p.innerText = userInput;
+
+        toDOList.appendChild(li);
+        li.appendChild(p);
+        li.appendChild(doneBtn);
+
+        doneBtn.addEventListener("click", e => {
+           // e.target.parentElement.remove();
+            toDOList.removeChild(li);
+        });
+    });
 
 }) ();
