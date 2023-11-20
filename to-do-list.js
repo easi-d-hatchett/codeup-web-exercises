3933881a0312f2c650236dee5c7d18f9adb5f6b2
Easi Hatchett <easi.hatchett3@gmail.com>
2023-11-20 11:40:08 -0500

    to-do-list

---
+++ to-do-list.js
@@ -0,0 +1,15 @@
+"use strict";
+
+(() => {
+    const form = document.querySelector("#to-do-form")
+    const addBtn =document.querySelector("#to-do-form button")
+
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        const input =document.querySelector("#to-do");
+        const userInput
+
+    }
+
+
+}) ();
