3f5a4e5de4e1957b6192bf02e0966107ce39e522
Easi Hatchett <easi.hatchett3@gmail.com>
2023-12-03 11:38:57 -0500

    weather map js added zoom controls and addEvent listener

--- weather_map.js
+++ weather_map.js
@@ -6,28 +6,27 @@ const map = new mapboxgl.Map({
     center: [-84.386330, 33.7488 ], // starting position [lng, lat]
     zoom: 9, // starting zoom
 });
-map.addControl(new mapboxgl.NavigationControl());
-geocode("Hall annex 55 Trinity Avenue SW Atlanta, GA 30303", MB_KEY).then(coords => {
-    const marker = new mapboxgl.Marker()
-        .setLngLat(coords)
-        .addTo(map);
-});
+
 const lat = 33.7488;
- const lon = -84.387;
+const lon = -84.387;
+
+// added "zoom control" to the map
+map.addControl(new mapboxgl.NavigationControl());
+
+// Event Listener for  "select zoom" option
+document.getElementById('zoom-level').addEventListener('change', function (event) {
+    const zoomLevel = parseFloat(event.target.value);
+    map.setZoom(zoomLevel);
+});
+
+
+
+
 fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`)
   .then(res => res.json())
  .then(data => {
-       console.log(data);fetch(`https://api.openweathermap.org/data/2.5/weather?` +
-         `lat=33.473120&lon=-84.200960` + `&appid=${WEATHER_API_KEY}`)
-         .then( data => data.json())
-         .then( data=> {
-             console.log(data.list[0].main.temp)
-             for(let i = 0; i < data.list.length; i += 8) {
-                 const div =document.querySelector('#weather-info')
-                 const p = document.createElement('p')
-                 p.innerHTML = data.list[i].main.temp;
-                 div.appendChild(p)
-                 console.log(data.list[i].main.temp)
-             }
-         })
+       console.log(data);
  });
+
+
+
